2b1199f1ef2d426fe665046ad8d0c9aa37164069
c0ttonplant <thebestofther@gmail.com>
2024-08-25 23:07:02 -0600

    improve ram struct

---
+++ src/ram.zig
@@ -0,0 +1,32 @@
+/// create a ram device
+/// `offsetStart` and `offsetEnd` are inclusive
+pub fn ram(offsetStart: u16, offsetEnd: u16) type
+{
+    if(offsetEnd <= offsetStart) @compileError("The starting address of ram must be smaller than the ending address");
+
+    // add one to the difference for correct array length
+    const length: u32 = @as(u32, @intCast(offsetEnd - offsetStart)) + 1;
+
+    // returning a struct allows for comptime arrays with custom length
+    comptime return struct 
+    {
+        pub const startOffset: u16 = offsetStart;
+        pub const endOffset: u16 = offsetEnd;
+        pub const len = length;
+        pub var data = [_]u8{0} ** length;
+
+        /// read from memory onto bus
+        pub fn read(addr: u16) u8
+        {
+            if(addr <= startOffset or addr >= endOffset) return 0;
+            return data[addr - startOffset];
+        }
+
+        /// write from bus onto memory
+        pub fn write(addr: u16, dat: u8) void
+        {
+            if(addr < startOffset or addr > endOffset) return;
+            data[addr - startOffset] = dat;
+        }
+    };
+}
