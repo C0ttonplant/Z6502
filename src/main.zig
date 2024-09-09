6bd230515fce7154c26e5a6d602826742cc04cab
c0ttonplant <thebestofther@gmail.com>
2024-09-08 19:15:33 -0600

    set gobal var to const

--- src/main.zig
+++ src/main.zig
@@ -8,28 +8,28 @@ var program = @embedFile("test_printing.bin");
 
 pub fn main() !void
 {
-    @memcpy(&bus.ram.data, program);
+    @memcpy(&bus.sysRam.data, program);
 
     std.debug.print("begin\n", .{});
 
     cpu_6502.reset();
 
-    while (true)
+    while (true) 
     {
         cpu_6502.clock();
     }
 }
 
-fn dumpVirtualMemory() !void
+fn dumpVirtualMemory() !void 
 {
     var cwd = fs.cwd();
-    
+
     var f = try cwd.createFile("vMemory.bin", .{});
     defer f.close();
 
     try f.writeAll(&bus.ram.data);
 
-    // for (0..0x10000) |i| 
+    // for (0..0x10000) |i|
     // {
     //     _ = try f.write(&[1]u8{cpu_6502.read(@intCast(i))});
     // }
